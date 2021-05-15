c87a50b22c36d831359bf1cf4ae9de58c1e7858c
harshgupta-taazaa <harsh.gupta@taazaa.com>
2021-05-15 23:24:10 +0530

    SolutionUsingLcmAndDivision

---
+++ TwoJoggersKATA1/TwoJoggersKATA1/Joggers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoJoggersKATA1
+{
+    class Joggers
+    {
+        public Tuple<int, int> Joggerss(int num1, int num2)
+        {
+            int LcmOfLaps = Lcm(num1, num2);
+            var output = Tuple.Create(LcmOfLaps / num1, LcmOfLaps / num2);
+            return output;
+
+        }
+        public int Lcm(int a, int b)
+        {
+            int num1, num2;
+            if (a > b)
+            {
+                num1 = a; num2 = b;
+            }
+            else
+            {
+                num1 = b; num2 = a;
+            }
+
+            for (int i = 1; i <= num2; i++)
+            {
+                if ((num1 * i) % num2 == 0)
+                {
+                    return i * num1;
+                }
+            }
+            return num2;
+        }
+    }
+}
+
---
+++ TwoJoggersKATA1/TwoJoggersKATA1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TwoJoggersKATA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            Joggers j1 = new Joggers();
+            Console.WriteLine(j1.Joggerss(3, 5));
+        }
+    }
+}
